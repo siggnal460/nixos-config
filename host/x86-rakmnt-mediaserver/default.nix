710ab64fd5db9908b73dfaf85afe9f638b02327f
siggnal <siggnal@proton.me>
2024-12-10 18:16:15 -0700

    Corrected media server hostname

--- host/x86-rakmnt-mediaserver/default.nix
+++ host/x86-rakmnt-mediaserver/default.nix
@@ -2,7 +2,7 @@
   imports = [ ./hardware-configuration.nix ];
 
   networking = {
-    hostName = "x86-racmnt-mediaserv";
+    hostName = "x86-rakmnt-mediaserver";
     #interface.eth0.ipv4.addresses = [{
     #  address = "10.4.0.1";
     #  prefixLength = 12;
