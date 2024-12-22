fed8b047422298dcb59c0044edf9cca5a8c3b541
siggnal <siggnal@proton.me>
2024-12-22 11:19:21 -0700

    Configured temporary static IP

--- host/x86-atxtwr-computeserver/default.nix
+++ host/x86-atxtwr-computeserver/default.nix
@@ -10,13 +10,18 @@
 
   networking = {
     hostName = "x86-atxtwr-computeserver";
-    #interfaces.enp7s0 = {
-    #useDHCP = false;
-    #ipv4.addresses = [{
-    #address = "10.6.0.1";
-    #prefixLength = 12;
-    #}];
-    #};
+    interfaces.enp8s0 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "10.0.0.10";
+          prefixLength = 12;
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "10.0.0.1";
+      interface = "enp8s0";
+    };
   };
-
 }
