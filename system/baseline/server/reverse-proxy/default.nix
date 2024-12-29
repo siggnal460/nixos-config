f19ed2912a39186126502b1ac6da8093b445f7ea
siggnal <siggnal@proton.me>
2024-12-29 11:24:18 -0700

    Got Jellyfin reverse proxy to work

--- system/baseline/server/reverse-proxy/default.nix
+++ system/baseline/server/reverse-proxy/default.nix
@@ -1,10 +1,31 @@
+{ pkgs, ... }:
+let
+  htmlFile = ./files/index.html;
+in
 {
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
   services.caddy = {
     enable = true;
     virtualHosts = {
-      "localhost" = {
+      "gappyland.org" = {
         extraConfig = ''
-          				  respond "Hello, world!"
+          					encode gzip
+          					file_server
+          					root * ${
+                 pkgs.runCommand "populateCaddyHtml" { } ''
+                           mkdir "$out"
+                   				echo "${builtins.readFile htmlFile}" > "$out/index.html"
+                             				''
+               }
+          					'';
+      };
+      "media.gappyland.org" = {
+        extraConfig = ''
+          				  reverse_proxy /jellyfin* 10.0.0.7:8096
           				'';
       };
     };
