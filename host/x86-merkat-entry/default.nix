840f74f53a7dad1337e059eb7a2229eb7963d419
siggnal <siggnal@proton.me>
2025-02-23 11:07:22 -0700

    Fixed Beszel agent config for entry server

--- host/x86-merkat-entry/default.nix
+++ host/x86-merkat-entry/default.nix
@@ -11,20 +11,17 @@
 
   virtualisation.oci-containers.containers = {
     beszel-agent = {
-      image = "henrygd/beszel-agent";
+      image = "docker.io/henrygd/beszel-agent";
       autoStart = true;
       labels = {
         "io.containers.autoupdate" = "registry";
       };
-      ports = [
-        "8090:8090"
-      ];
       volumes = [
-        "/var/run/docker.sock:/var/run/docker.sock:ro"
+        "/var/run/podman/podman.sock:/var/run/docker.sock:ro"
       ];
       environment = {
         PORT = "45876";
-        KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvz32rDPSRNQlyuSXO3wvz6MlbMdiMUeJvQcgtg+5T5";
+        KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXz3a6nV8xxYD5tomKiPul/RTuaAK2s51cGzxgv/X1s";
       };
       extraOptions = [
         "--name=beszel-agent"
