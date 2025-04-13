9ceaea3a49a2faca71659137dd6816e2d40455dc
siggnal460 <siggnal@proton.me>
2025-04-13 13:59:16 -0600

    Replaced transmission with deluge

--- system/baseline/server/download-client/default.nix
+++ system/baseline/server/download-client/default.nix
@@ -1,20 +1,20 @@
 {
   systemd.tmpfiles.rules = [
-    "d /export/media/torrents 0770 transmission media"
-    "d /export/media/torrents/complete 0770 transmission media"
-    "d /export/media/torrents/complete/anime 0770 transmission media"
-    "d /export/media/torrents/complete/anime-movies 0770 transmission media"
-    "d /export/media/torrents/complete/books 0770 transmission media"
-    "d /export/media/torrents/complete/tvshows 0770 transmission media"
-    "d /export/media/torrents/complete/movies 0770 transmission media"
-    "d /export/media/torrents/incomplete 0770 transmission media"
+    "d /export/media/torrents 0770 deluge media"
+    "d /export/media/torrents/complete 0770 deluge media"
+    "d /export/media/torrents/complete/anime 0770 deluge media"
+    "d /export/media/torrents/complete/anime-movies 0770 deluge media"
+    "d /export/media/torrents/complete/books 0770 deluge media"
+    "d /export/media/torrents/complete/tvshows 0770 deluge media"
+    "d /export/media/torrents/complete/movies 0770 deluge media"
+    "d /export/media/torrents/incomplete 0770 deluge media"
     "d /export/media/usenet 0770 nzbget media"
     "d /export/media/usenet/completed 0770 nzbget media"
     "d /export/media/usenet/completed/Anime 0770 nzbget media"
     "d /export/media/usenet/completed/Movies 0770 nzbget media"
     "d /export/media/usenet/completed/Series 0770 nzbget media"
     "d /export/media/youtube 0770 pinchflat media"
-    "d /etc/transmission 0750 transmission wheel"
+    "d /etc/deluge 0750 deluge wheel"
     "d /etc/nzbget 0750 nzbget wheel"
     "d /etc/pinchflat 0750 pinchflat wheel"
   ];
@@ -31,7 +31,7 @@
   ];
 
   users.users = {
-    transmission = {
+    deluge = {
       uid = 710;
       isSystemUser = true;
       group = "media";
@@ -72,29 +72,28 @@
       ];
     };
 
-    transmission = {
-      image = "lscr.io/linuxserver/transmission:latest";
+    deluge = {
+      image = "lscr.io/linuxserver/deluge:latest";
       autoStart = true;
       labels = {
         "io.containers.autoupdate" = "registry";
       };
       ports = [
-        "9091:9091"
-        "51413:51413"
-        "51413:51413/udp"
+        "8112:8112"
+        "49154:6881"
+        "49154:6881/udp"
       ];
       environment = {
         PUID = "710";
         PGID = "982";
         TZ = "America/Denver";
-        PEERPORT = "51413";
       };
       volumes = [
-        "/export/media/torrents:/data/torrents"
-        "/etc/transmission:/config"
+        "/export/media/torrents:/downloads"
+        "/etc/deluge:/config"
       ];
       extraOptions = [
-        "--name=transmission"
+        "--name=deluge"
       ];
     };
 
--- system/baseline/server/openvpn-client/default.nix
+++ system/baseline/server/openvpn-client/default.nix
@@ -7,33 +7,33 @@
         autoStart = true;
         updateResolvConf = false;
         config = ''
-                    					client
-                    					dev tun
-                    					remote europe3.vpn.airdns.org 443
-                    					resolv-retry infinite
-          										pull-filter ignore "dhcp-option DNS"
-          										dhcp-option DNS 8.8.8.8
-                    					nobind
-                    					persist-key
-                    					persist-tun
-                    					auth-nocache
-                    					verb 3
-                    					explicit-exit-notify 5
-                    					rcvbuf 262144
-                    					sndbuf 262144
-                    					push-peer-info
-                    					setenv UV_IPV6 yes
-                    					ca "/root/.vpn/ca.crt"
-                    					cert "/root/.vpn/aaron.crt"
-                    					key "/root/.vpn/aaron.key"
-                    					remote-cert-tls server
-                    					comp-lzo no
-                    					data-ciphers AES-256-GCM:AES-256-CBC:AES-192-GCM:AES-192-CBC:AES-128-GCM:AES-128-CBC
-                    					data-ciphers-fallback AES-256-CBC
-                    					proto udp
-                    					tls-crypt "/root/.vpn/tls-crypt.key"
-                    					auth SHA512
-                    				'';
+          	client
+          	dev tun
+          	remote europe3.vpn.airdns.org 443
+          	resolv-retry infinite
+          	pull-filter ignore "dhcp-option DNS"
+          	dhcp-option DNS 8.8.8.8
+          	nobind
+          	persist-key
+          	persist-tun
+          	auth-nocache
+          	verb 3
+          	explicit-exit-notify 5
+          	rcvbuf 262144
+          	sndbuf 262144
+          	push-peer-info
+          	setenv UV_IPV6 yes
+          	ca "/root/.vpn/ca.crt"
+          	cert "/root/.vpn/aaron.crt"
+          	key "/root/.vpn/aaron.key"
+          	remote-cert-tls server
+          	comp-lzo no
+          	data-ciphers AES-256-GCM:AES-256-CBC:AES-192-GCM:AES-192-CBC:AES-128-GCM:AES-128-CBC
+          	data-ciphers-fallback AES-256-CBC
+          	proto udp
+          	tls-crypt "/root/.vpn/tls-crypt.key"
+          	auth SHA512
+        '';
         up = "echo nameserver $nameserver | ${pkgs.openresolv}/sbin/resolvconf -m 0 -a $dev";
         down = "${pkgs.openresolv}/sbin/resolvconf -d $dev";
       };
