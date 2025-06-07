2644789adcedbba2ea2c1e36ba70eb3f28d294ba
siggnal460 <siggnal@proton.me>
2025-06-07 07:27:29 -0600

    Removed unneeded GNOME config

--- system/de/gnome-minimal/default.nix
+++ system/de/gnome-minimal/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
-  services.xserver = {
-    enable = true;
+	services = {
+    xserver.enable = true;
     displayManager.gdm = {
       enable = true;
       autoSuspend = false;
