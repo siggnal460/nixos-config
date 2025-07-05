9d0a14ba65d988dd0202bcda853cebfa124244ec
siggnal460 <siggnal@proton.me>
2025-07-05 12:09:51 -0600

    Added to raspberry-pi config

--- system/hardware/raspi4/default.nix
+++ system/hardware/raspi4/default.nix
@@ -2,6 +2,32 @@
 {
   imports = [ inputs.nixos-hardware.nixosModules.raspberry-pi-5 ];
 
+	boot = {
+		# The Linux kernel version
+		kernelPackages = pkgs.linuxPackages_rpi4;
+		# Options to enable serial console and more
+		kernelParams = [
+			"8250.nr_uarts=1"
+			"console=tyAMA0,115200"
+			"console=tty1"
+			"cma=128M"
+		];
+
+		# The bootloader
+		loader = {
+			raspberryPi = {
+				enable = true;
+				version = 4;
+			};
+
+			# Use extlinux, whereas the NixOS default is Grub
+			grub.enable = false;
+
+			# Enables the generation of /boot/extlinux/extlinux.conf
+			generic-extlinux-compatible.enable = true;
+		};
+	};
+
 	hardware = {
     raspberry-pi."4".apply-overlays-dtmerge.enable = true;
     deviceTree = {
@@ -13,5 +39,6 @@
   environment.systemPackages = with pkgs; [
     libraspberrypi
     raspberrypi-eeprom
+		raspberrypifw
   ];
 }
