c3a5dd7faf9e5615c3da1ea229532f29d1282636
siggnal460 <siggnal@proton.me>
2025-07-20 16:46:30 -0600

    Formatting

--- system/hardware/raspi4/default.nix
+++ system/hardware/raspi4/default.nix
@@ -1,9 +1,11 @@
 { lib, ... }:
 {
-  swapDevices = [ {
-    device = "/var/lib/swapfile";
-    size = 5*1024; # pushes the 2GB model to 8GB total including zram, necessary for builds to not fail
-  } ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 5 * 1024; # pushes the 2GB model to 8GB total including zram, necessary for builds to not fail
+    }
+  ];
 
   nix.settings.cores = 2;
 
@@ -13,13 +15,14 @@
       grub.enable = lib.mkForce false;
       systemd-boot.enable = lib.mkForce false;
       generic-extlinux-compatible = {
-			  enable = lib.mkForce true;
-				configurationLimit = lib.mkForce 2;
-			};
+        enable = lib.mkForce true;
+        configurationLimit = lib.mkForce 2;
+      };
     };
   };
 
-  services.clamav = { # way too much RAM
+  services.clamav = {
+    # way too much RAM
     daemon.enable = lib.mkForce false;
     updater.enable = lib.mkForce false;
     fangfrisch.enable = lib.mkForce false;
