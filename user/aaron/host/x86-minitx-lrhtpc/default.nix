fbec2107d93926a7a072dc7ce1ab5cf7f247366e
siggnal460 <siggnal@proton.me>
2025-09-21 10:52:12 -0600

    Fixed LRHTPC scale

--- user/aaron/host/x86-minitx-lrhtpc/default.nix
+++ user/aaron/host/x86-minitx-lrhtpc/default.nix
@@ -2,7 +2,7 @@
 {
   home-manager.users.aaron = {
     wayland.windowManager.hyprland.settings = {
-      monitor = "HDMI-A-2, 2440x1440@120, 0x0, 3, bitdepth, 10, cm, hdr";
+      monitor = "HDMI-A-2, 2440x1440@120, 0x0, 3.2, bitdepth, 10, cm, hdr";
     };
   };
 }
