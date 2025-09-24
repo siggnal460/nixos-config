55c48e3c50ef6ca3edb2b86810c6ee3be5d57181
siggnal460 <siggnal@proton.me>
2025-09-23 19:58:42 -0600

    Fixed swayidle

--- user/aaron/host/x86-minitx-lrhtpc/default.nix
+++ user/aaron/host/x86-minitx-lrhtpc/default.nix
@@ -2,27 +2,27 @@
 {
   home-manager.users.aaron = {
     wayland.windowManager.hyprland.settings = {
-      monitor = "HDMI-A-2, 3840x2160@60.00, 0x0, 3, bitdepth, 10, cm, hdr";
+      monitor = "HDMI-A-2, 3840x2160@120.00, 0x0, 3, bitdepth, 10, cm, hdr";
+    };
 
-      services.swayidle = {
-        extraArgs = [ "-w" ];
-        events = [
-          {
-            event = "before-sleep";
-            command = "${pkgs.swaylock}/bin/swaylock -fF";
-          }
-        ];
-        timeouts = [
-          {
-            timeout = 600; # 10min
-            command = "${pkgs.swaylock}/bin/swaylock -f -c 000000"; # Change color to black
-          }
-          {
-            timeout = 2700; # 45min
-            command = "${pkgs.systemd}/bin/systemctl sleep";
-          }
-        ];
-      };
+    services.swayidle = {
+      extraArgs = [ "-w" ];
+      events = [
+        {
+          event = "before-sleep";
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+      ];
+      timeouts = [
+        {
+          timeout = 600; # 10min
+          command = "${pkgs.swaylock}/bin/swaylock -f -c 000000"; # Change color to black
+        }
+        {
+          timeout = 2700; # 45min
+          command = "${pkgs.systemd}/bin/systemctl sleep";
+        }
+      ];
     };
   };
 }
