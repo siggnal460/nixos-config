e34d08e3db39d6541ace5b8d2f1ea2e3d22a2063
siggnal460 <siggnal@proton.me>
2025-11-11 10:10:17 -0700

    Added simple Vikunja module

---
+++ system/baseline/server/vikunja/default.nix
@@ -0,0 +1,43 @@
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/vikunja/files 0770 vikunja wheel"
+    "d /var/lib/vikunja/db 0770 vikunja wheel"
+	];
+
+  users.users = {
+    vikunja = {
+      uid = 600;
+      isSystemUser = true;
+      group = "vikunja";
+    };
+	};
+
+  users.groups = {
+    vikunja = {
+      gid = 600;
+    };
+	};
+
+  virtualisation.oci-containers.containers = {
+    vikunja = {
+      image = "docker.io/vikunja/vikunja:latest";
+      autoStart = true;
+      labels = {
+        "io.containers.autoupdate" = "registry";
+      };
+      ports = [
+        "3456:3456"
+      ];
+      environment = {
+        VIKUNJA_SERVICE_PUBLICURL = "https://tasks.gappyland.org";
+      };
+      volumes = [
+        "/var/lib/vikunja/files:/app/vikunja/files"
+				"/var/lib/vikunja/db:/db"
+      ];
+      extraOptions = [
+        "--name=vikunja"
+      ];
+    };
+
+}
