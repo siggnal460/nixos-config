c061dae3237d10854d7e2fb917d9543a8dfc69cc
siggnal460 <siggnal@proton.me>
2026-01-01 15:11:39 -0700

    Opened port 80 (was causing certs to not autorenew) and added some security settings

--- system/baseline/server/reverse-proxy/default.nix
+++ system/baseline/server/reverse-proxy/default.nix
@@ -5,7 +5,7 @@ let
 in
 {
   networking.firewall.allowedTCPPorts = [
-    #80
+    80
     443
   ];
 
@@ -19,87 +19,159 @@ in
       '';
 
       "auth.${domain}".extraConfig = ''
-        reverse_proxy x86-merkat-auth:9091
+        			  import main
+        				import headers
+
+                reverse_proxy x86-merkat-auth:9091
       '';
 
       "users.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy x86-merkat-auth:${toString ldap_cfg.settings.http_port}
+                reverse_proxy x86-merkat-auth:${toString ldap_cfg.settings.http_port}
       '';
 
       "media.${domain}".extraConfig = ''
-        reverse_proxy x86-rakmnt-mediaserver:8096
+        			  import main
+        				import headers
+
+                reverse_proxy x86-rakmnt-mediaserver:8096
       '';
 
       "monitoring.${domain}".extraConfig = ''
-        import auth
+                import auth
+        			  import main
+        				import headers
 
-        reverse_proxy x86-merkat-auth:8090
+                reverse_proxy x86-merkat-auth:8090
       '';
 
       "requests.${domain}".extraConfig = ''
-        reverse_proxy x86-rakmnt-mediaserver:5055
+        			  import main
+        				import headers
+
+                reverse_proxy x86-rakmnt-mediaserver:5055
       '';
 
       "roms.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy x86-rakmnt-mediaserver:81
+                reverse_proxy x86-rakmnt-mediaserver:81
       '';
 
       "books.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy x86-rakmnt-mediaserver:25600
+                reverse_proxy x86-rakmnt-mediaserver:25600
       '';
 
       "textgen.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy x86-atxtwr-computeserver:3000
+                reverse_proxy x86-atxtwr-computeserver:3000
       '';
 
       "imagegen.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy x86-atxtwr-computeserver:9091
+                reverse_proxy x86-atxtwr-computeserver:9091
       '';
 
       "gitea.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy x86-atxtwr-computeserver:3003
+                reverse_proxy x86-atxtwr-computeserver:3003
       '';
 
       "home.${domain}".extraConfig = ''
-        import auth
+        				import main
+                import auth
+        				import headers
 
-        reverse_proxy arm-raspi4-home:8123
+                reverse_proxy arm-raspi4-home:8123
       '';
 
       "recipes.${domain}".extraConfig = ''
-        reverse_proxy x86-rakmnt-mediaserver:5413
+        			  import main
+                import auth
+        				import headers
+
+        			  header Host $http_host
+        			  header X-Forwarded-Proto $scheme;
+
+        				handle_path /media/* {
+        					root * /www/tandoor
+        					file_server
+        				}
+
+                reverse_proxy x86-rakmnt-mediaserver:5413
       '';
 
       "cloud.${domain}".extraConfig = ''
-        import auth
+        			  import main
+                import auth
+        				import headers
 
-        reverse_proxy https://x86-rakmnt-mediaserver:443 {
-          transport http {
-        	  tls
-        		tls_insecure_skip_verify
-        	}
-        }
+                reverse_proxy https://x86-rakmnt-mediaserver:443 {
+                  transport http {
+                	  tls
+                		tls_insecure_skip_verify
+                	}
+                }
       '';
     };
     extraConfig = ''
-      	(auth) {
-      	  forward_auth x86-merkat-auth:9091 {
-      		  uri /api/authz/forward-auth
-      			copy_headers remote-user remote-groups remote-email remote-name
-      		}
-      	}
+            	(auth) {
+            	  forward_auth x86-merkat-auth:9091 {
+            		  uri /api/authz/forward-auth
+            			copy_headers remote-user remote-groups remote-email remote-name
+            		}
+            	}
+
+      				(main) {
+      					
+      					respond /robots.txt 200 {
+      						body "User-agent: *
+      						Disallow: /
+      						
+      						User-agent: AdsBot-Google
+      						Disallow: /
+
+      						User-agent: AdsBot-Google-Mobile
+      						Disallow: /"
+
+      						close
+      					}
+      				}
+
+      				(headers) {
+      					header {
+      						-server #anonymizes Caddy
+
+      						# disable FLoC tracking
+      						Permissions-Policy interest-cohort=()
+
+      						# enable HSTS
+      						Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
+
+      						X-Content-Type-Options "nosniff" # disable clients from sniffing the media type
+
+      						# clickjacking protection
+      						X-Frame-Options DENY
+      					}
+      				}
     '';
   };
 }
